904e8cc1ac33fd663f9d36b943add21be309663e
chekist <georg4932@mail.ru>
2024-10-23 01:24:15 +1000

    configure table columns, create table relations

--- src/sales/models.py
+++ src/sales/models.py
@@ -1,23 +1,46 @@
-from sqlalchemy import MetaData, Table
+import datetime
+
+from sqlalchemy import MetaData, Table, Column, Integer, Float, String, ForeignKey, DateTime
 
 metadata = MetaData()
 
-product = Table(
-    'product',
-    metadata
+city = Table(
+    'city',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String, nullable=False)
 )
 
 store = Table(
     'store',
-    metadata
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String, nullable=False),
+    Column('city_id', Integer, ForeignKey('city.id'))
+
 )
 
-city = Table(
-    'city',
-    metadata
+product = Table(
+    'product',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String, nullable=False),
+    Column('price', Float(precision=32, decimal_return_scale=2), nullable=False)
 )
 
 sale = Table(
     'sale',
-    metadata
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('store_id', Integer, ForeignKey('store.id')),
+    Column('total_amount', Float(precision=32, decimal_return_scale=2), nullable=False),
+    Column('sale_date', DateTime, default=datetime.datetime.now(datetime.UTC))
 )
+
+sale_product = Table('SaleProduct',
+                     metadata,
+                     Column('product_id', Integer, ForeignKey('product.id')),
+                     Column('sale_id', Integer, ForeignKey('sale.id')),
+                     Column('quantity', Integer, nullable=True),
+                     Column('total_price', Float(precision=32, decimal_return_scale=2), nullable=False)
+                     )
