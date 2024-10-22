4be75aef7dd00260fc2ce6e22abe8503b77f0966
chekist <georg4932@mail.ru>
2024-10-22 19:56:16 +1000

    update src/config.py: resolve BASE_DIR, check exists .env, get from enviroment db params

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_path = os.path.join(BASE_DIR, '.env')
+
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+DB_NAME = os.environ.get('DB_NAME')
