ebc2483aaf8e5f4dbea001e732efd950c539aa26
chekist <georg4932@mail.ru>
2024-10-23 18:09:11 +1000

    src/sales/models.py refactore columns: id --> <table_name>_id;

--- src/sales/models.py
+++ src/sales/models.py
@@ -7,37 +7,37 @@ metadata = MetaData()
 city = Table(
     'city',
     metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String, nullable=False)
+    Column('city_id', Integer, primary_key=True, autoincrement=True),
+    Column('city_name', String, nullable=False)
 )
 
 store = Table(
     'store',
     metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String, nullable=False),
-    Column('city_id', Integer, ForeignKey('city.id'))
+    Column('store_id', Integer, primary_key=True, autoincrement=True),
+    Column('store_name', String, nullable=False),
+    Column('city_id', Integer, ForeignKey('city.city_id'))
 
 )
 
 product = Table(
     'product',
     metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String, nullable=False),
+    Column('product_id', Integer, primary_key=True, autoincrement=True),
+    Column('product_name', String, nullable=False),
     Column('price', DECIMAL(precision=10, scale=2), nullable=False)
 )
 
 sale = Table(
     'sale',
     metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sale_id', Integer, primary_key=True, autoincrement=True),
     Column('operation_uid', Integer, nullable=False),
     Column('sale_date', DateTime, default=datetime.now(UTC)),
     Column('quantity', Integer, nullable=False),
     Column('total_price', DECIMAL(precision=10, scale=2), nullable=False),
-    Column('store_id', Integer, ForeignKey('store.id')),
-    Column('product_id', Integer, ForeignKey('product.id'))
+    Column('store_id', Integer, ForeignKey('store.store_id')),
+    Column('product_id', Integer, ForeignKey('product.product_id'))
 
 )
 
