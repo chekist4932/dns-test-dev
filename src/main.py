2deb70578dfc106aa270eb79dee0515b4467a928
chekist <georg4932@mail.ru>
2024-10-24 17:46:12 +1000

    create src/exception.py: define register_exception_handlers[sqlalchemy_exception_handler, http_exception_handler]; import to src/main.py;

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from src.sales.router import router as router_sales
+from src.exceptions import register_exception_handlers
 
 app = FastAPI(title='dns-test-dev')
+register_exception_handlers(app)
 
 app.include_router(router_sales)
